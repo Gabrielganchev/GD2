dbb1617c248cd9bd9461973f106037bb16b5d944
Gabriel Ganchev <gabriel_ganchev@abv.bg>
2015-09-30 01:26:31 +0300

    Pygame dev

---
+++ Survivor.py
@@ -0,0 +1,121 @@
+"""
+The Survivor file contorls the survivor and his or her movement and actions.
+"""
+
+import pygame
+from Character import Character
+from Character import Direction
+from Weapon import Weapon
+
+
+__author__ = 'G.Ganchev'
+
+
+class Survivor(Character):
+
+    _velocity = 8
+
+    START_HEALTH = 1000
+
+    def __init__(self, x, y):
+
+        self.health = Survivor.START_HEALTH
+        self.current_weapon = Weapon(Weapon.PISTOL)
+        self.direction = Direction.WEST
+        self.img = pygame.image.load('images/survivor/survivor_w.png')
+
+        Character.__init__(self, x, y)
+
+    def get_bullet_type_based_on_weapon(self):
+
+        return self.current_weapon.weapon_type
+
+
+    """
+    movement determines if the player should move,
+    and what direction to move in
+    """
+
+    def movement(self):
+
+        if self.isMoving():  # Target is set
+
+            x_destination = self.x - self.dx
+            y_destination = self.y - self.dy
+
+            if x_destination < 0:  # --->
+                self.x += Survivor._velocity
+
+            elif x_destination > 0:  # <----
+                self.x -= Survivor._velocity
+
+            if y_destination > 0:  # up
+                self.y -= Survivor._velocity
+
+            elif y_destination < 0:  # down
+                self.y += Survivor._velocity
+
+            if x_destination == 0 and y_destination == 0:
+                self.dx, self.dy = 0, 0
+
+    def draw(self, screen):
+
+        gun_img = Weapon.get_weapon_image( self.current_weapon )
+
+        if self.direction == Direction.WEST:
+            screen.blit(gun_img, (self.centerx - gun_img.get_rect().width, self.centery))
+
+        elif self.direction == Direction.EAST:
+            gun_img = pygame.transform.flip(gun_img, True, False)
+            screen.blit(gun_img, (self.centerx, self.centery))
+
+        elif self.direction == Direction.SOUTH:
+            gun_img = pygame.transform.rotate(gun_img, 90)  # CCW
+            screen.blit(gun_img, (self.centerx, self.centery))
+
+        # weapon drawn below survivor
+        elif self.direction == Direction.NORTH:
+            south = pygame.transform.rotate(gun_img, 90)
+            gun_img = pygame.transform.flip(south, False, True)
+            screen.blit(gun_img, (self.centerx, self.centery - gun_img.get_rect().height))
+
+        screen.blit(self.img, (self.x, self.y))
+
+
+    """ Changes player direction image"""
+    def rotate(self, direction):
+
+        path = 'images/survivor/survivor_'
+        png = '.png'
+
+        if direction == Direction.NORTH:
+            if self.direction != Direction.NORTH:
+                self.direction = Direction.NORTH
+                self.img = pygame.image.load(path + self.direction + png)
+
+        if direction == Direction.SOUTH:
+            if self.direction != Direction.SOUTH:
+                self.direction = Direction.SOUTH
+                self.img = pygame.image.load(path + self.direction + png)
+
+        if direction == Direction.EAST:
+            if self.direction != Direction.EAST:
+                self.direction = Direction.EAST
+                self.img = pygame.image.load(path + self.direction + png)
+
+        if direction == Direction.WEST:
+            if self.direction != Direction.WEST:
+                self.direction = Direction.WEST
+                self.img = pygame.image.load(path + self.direction + png)
+
+    # Cycle through the weapons
+    def cycle_weapon(self):
+
+        if self.current_weapon.weapon_type == Weapon.PISTOL:
+            self.current_weapon = Weapon(Weapon.SHOTGUN)
+
+        elif self.current_weapon.weapon_type == Weapon.SHOTGUN:
+            self.current_weapon = Weapon(Weapon.AUTOMATIC)
+
+        elif self.current_weapon.weapon_type == Weapon.AUTOMATIC:
+            self.current_weapon = Weapon(Weapon.PISTOL)
---
+++ worldCreator.py
@@ -0,0 +1,79 @@
+"""
+
+This File is intended to handle everything to do with create a world
+
+"""
+
+from Tile import Tile
+from Zombie import Zombie
+
+__author__ = 'G.Ganchev'
+
+WORLD_WIDTH, WORLD_HEIGHT = 0, 0
+
+# CharacterNotDefined simply means that map.txt file contains a character we have not defined
+class CharacterNotDefined(Exception): pass
+
+
+def create_world(world_text_file):
+
+    global WORLD_HEIGHT, WORLD_WIDTH
+
+    map_ = open(world_text_file)
+
+    # Loop through the entire level file, reading each line at a time
+    for column, line in enumerate(map_.readlines()):
+
+        WORLD_HEIGHT += 1
+        WORLD_WIDTH = len(line)
+
+        for row, character in enumerate(line):
+
+            if character == '\n': continue
+
+            tile_type = None
+
+            # Checks for each block Type
+            if character == Tile.Type.WALL:
+                Tile(row * Tile.TILE_SIZE, column * Tile.TILE_SIZE, Tile.Type.WALL)
+
+            elif character == Tile.Type.FLOOR:
+                Tile(row * Tile.TILE_SIZE, column * Tile.TILE_SIZE, Tile.Type.FLOOR)
+
+            # Marks a zombie spawn tile
+            elif character == Zombie.SPAWN_ZONE:
+                Zombie.spawn_tiles.append(Tile.total_tiles)
+                Tile(row * Tile.TILE_SIZE, column * Tile.TILE_SIZE, Tile.Type.FLOOR)
+
+            else:
+                errorStr = "\n\nCharacter '{0}' is not defined in Tile.Type.* \n"
+                raise CharacterNotDefined(errorStr.format(character))
+
+    print WORLD_WIDTH, WORLD_HEIGHT
+
+    # Might not be a bad idea to close a file after you open it ;P
+    map_.close()
+
+def get_dimension():
+
+    if WORLD_WIDTH != 0 and WORLD_HEIGHT != 0:
+        return (WORLD_WIDTH, WORLD_HEIGHT)
+    else:
+        raise Exception("World not yet created")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
