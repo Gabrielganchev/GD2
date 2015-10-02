bd2cc9e30ccf64a757395c33b1814af7aa7ca079
Gabriel Ganchev <gabriel_ganchev@abv.bg>
2015-10-02 11:20:45 +0300

    Pygame dev

--- interaction.py
+++ interaction.py
@@ -1,15 +1,11 @@
-
-
 import pygame, sys
 from tileC import Tile
 
+def interaction(screen, survivor):
 
-def interaction(screen,survivor):
-
-    Mpos = pygame.mouse.get_pos() # [x,y]  mouse position
-
-    Mx = Mpos[0]/Tile.width
-    My = Mpos[1]/Tile.height
+    Mpos = pygame.mouse.get_pos() # [x, y]
+    Mx = Mpos[0] / Tile.width
+    My = Mpos[1] / Tile.height
 
     for event in pygame.event.get():
 
@@ -17,42 +13,47 @@ def interaction(screen,survivor):
             pygame.quit()
             sys.exit()
 
-
-
         if event.type == pygame.MOUSEBUTTONDOWN:
             for tile in Tile.List:
-                if tile.x == (Mx *Tile.width) and tile.y == (My *Tile.width):
+                if tile.x == (Mx * Tile.width) and tile.y == (My * Tile.width):
                     tile.type = 'solid'
                     tile.walkable = False
                     break
 
-
-
-        if event.type== pygame.KEYDOWN:
-
-            if event.key == pygame.K_w: # notrh
-                future__tile_number = survivor.get_number() - Tile.V
-                if Tile.get_tile(future__tile_number).walkable:
-                    survivor.y -= survivor.height
+    keys = pygame.key.get_pressed()
 
 
 
-            if event.key == pygame.K_s: # South
-                future__tile_number = survivor.get_number() + Tile.V
-                if Tile.get_tile(future__tile_number).walkable:
-                    survivor.y += survivor.height
+    if keys[pygame.K_w]: # North
+        future_tile_number = survivor.get_number() - Tile.V
+        if future_tile_number in range(1, Tile.total_tiles + 1):
+            future_tile = Tile.get_tile(future_tile_number)
+            if future_tile.walkable:
+                survivor.set_target(future_tile)
+                # survivor.y -= survivor.height
 
+    if keys[pygame.K_s]: # South
+        future_tile_number = survivor.get_number() + Tile.V
+        if future_tile_number in range(1, Tile.total_tiles + 1):
+            future_tile = Tile.get_tile(future_tile_number)
+            if future_tile.walkable:
+                survivor.set_target(future_tile)
+                # survivor.y += survivor.height
 
+    if keys[pygame.K_a]: # West
+        future_tile_number = survivor.get_number() - Tile.H
 
-            if event.key == pygame.K_a: # WEST
-                future__tile_number = survivor.get_number() - Tile.H
-                if Tile.get_tile(future__tile_number).walkable:
-                    survivor.x -= survivor.width
+        if future_tile_number in range(1, Tile.total_tiles + 1):
+            future_tile = Tile.get_tile(future_tile_number)
+            if future_tile.walkable:
+                survivor.set_target(future_tile)
+                # survivor.x -= survivor.width
 
+    if keys[pygame.K_d]: # East
+        future_tile_number = survivor.get_number() + Tile.H
+        if future_tile_number in range(1, Tile.total_tiles + 1):
+            future_tile = Tile.get_tile(future_tile_number)
+            if future_tile.walkable:
+                survivor.set_target(future_tile)
+                # survivor.x += survivor.width
 
-
-
-            if event.key == pygame.K_d: # East
-                future__tile_number = survivor.get_number() + Tile.H
-                if Tile.get_tile(future__tile_number).walkable:
-                    survivor.x += survivor.width
