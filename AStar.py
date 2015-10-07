2f422575064894ad8f9e21eefc250b8028c772b4
Gabriel Ganchev <gabriel_ganchev@abv.bg>
2015-10-07 11:09:20 +0300

    Pygame

---
+++ AStar.py
@@ -0,0 +1,192 @@
+# import pygame  # this is imported, but not used
+from Zombie import Zombie
+from Tile import Tile
+
+__author__ = 'G.Ganchev'
+
+
+def AStar(survivor, total_frames, FPS):
+
+    # half = Tile.width / 2  # this is assigned, but not used
+
+    N = -22
+    S = 22
+    E = 1
+    W = -1
+
+    NW = -23
+    NE = -21
+    SE = 23
+    SW = 21
+
+    for tile in Tile.list_:
+        tile.parent = None
+        tile.H, tile.G, tile.F = 0, 0, 0
+
+    def blocky(tiles, diagonals, surrounding_node):
+        if surrounding_node.number not in diagonals:
+            tiles.append(surrounding_node)
+        return tiles
+
+    def get_surrounding_tiles(base_node):
+
+        array = (
+            (base_node.number + N),
+            (base_node.number + NE),
+            (base_node.number + E),
+            (base_node.number + SE),
+            (base_node.number + S),
+            (base_node.number + SW),
+            (base_node.number + W),
+            (base_node.number + NW),
+            )
+
+        tiles = []
+
+        onn = base_node.number
+        diagonals = [onn + NE, onn + NW, onn + SE, onn + SW]
+
+        for tile_number in array:
+
+            surrounding_tile = Tile.get_tile(tile_number)
+
+            if tile_number not in list(range(1, Tile.total_tiles + 1)):
+                continue
+
+            if (surrounding_tile.walkable and
+                surrounding_tile not in closed_list):
+                # tiles.append(surrounding_tile) # Diagonal movement
+                tiles = blocky(tiles, diagonals, surrounding_tile)
+
+        return tiles
+
+    def G(tile):
+
+        diff = tile.number - tile.parent.number
+
+        if diff in (N, S, E, W):
+            tile.G = tile.parent.G + 10
+        elif diff in (NE, NW, SW, SE):
+            tile.G = tile.parent.G + 14
+
+    def H():
+        for tile in Tile.list_:
+            absX = abs(tile.x - survivor.x)
+            absY = abs(tile.y - survivor.y)
+            tile.H = 10 * (absX + absY) / Tile.TILE_SIZE
+
+    def F(tile):
+        # F = G + H
+        tile.F = tile.G + tile.H
+
+    def swap(tile):
+
+        open_list.remove(tile)
+        closed_list.append(tile)
+
+
+    def get_LFT():  # get Lowest F Value
+
+        F_Values = []
+        for tile in open_list:
+            F_Values.append(tile.F)
+
+        # Reverse List
+        o = open_list[::-1]
+
+        for tile in o:
+            if tile.F == min(F_Values):
+                return tile
+
+    def move_to_G_cost(LFT, tile):
+
+        GVal = 0
+        diff = LFT.number - tile.number
+
+        if diff in (N, S, E, W):
+            GVal = LFT.G + 10
+        elif diff in (NE, NW, SE, SW):
+            GVal = LFT.G + 14
+
+        return GVal
+
+    def loop():
+
+        LFT = get_LFT()
+
+        swap(LFT)
+        surrounding_nodes = get_surrounding_tiles(LFT)
+
+        for node in surrounding_nodes:
+
+            if node not in open_list:
+
+                open_list.append(node)
+                node.parent = LFT
+
+            elif node in open_list:
+
+                calculated_G = move_to_G_cost(LFT, node)
+                if calculated_G < node.G:
+
+                    node.parent = LFT
+                    G(node)
+                    F(node)
+
+        if open_list == [] or survivor.get_tile() in closed_list:
+            return
+
+        for node in open_list:
+            G(node)
+            F(node)
+
+        loop()
+
+    #TODO: add relevant comment here
+
+    for zombie in Zombie.list_:
+
+        if zombie.isMoving():  # zombie.dx != 0 or zombie.dy != 0:
+            continue
+
+        open_list = []
+        closed_list = []
+
+        zombie_tile = zombie.get_tile()
+        open_list.append(zombie_tile)
+
+        surrounding_nodes = get_surrounding_tiles(zombie_tile)
+
+        for node in surrounding_nodes:
+            node.parent = zombie_tile
+            open_list.append(node)
+
+        swap(zombie_tile)
+
+        H()
+
+        for node in surrounding_nodes:
+            G(node)
+            F(node)
+
+        loop()
+
+        return_tiles = []
+
+        parent = survivor.get_tile()
+
+        while True:
+
+            return_tiles.append(parent)
+
+            parent = parent.parent
+
+            if parent is None:
+                break
+
+            if parent.number == zombie.get_number():
+                break
+
+        if len(return_tiles) > 1:
+            next_tile = return_tiles[-1]
+            zombie.set_target(next_tile)
---
+++ Tile.py
@@ -0,0 +1,64 @@
+"""
+The Tile class is an object representation of a tile on the screen
+"""
+
+__author__ = 'G.Ganchev'
+
+import pygame
+
+class Tile(pygame.Rect):
+
+    floor_tile_img = pygame.image.load('images/tiles/surface_tile_gray.png')
+    wall_tile_img = pygame.image.load("images/tiles/dark_wall.png")
+
+    # Define all the different kinds of tiles that exist
+    class Type:
+        FLOOR = '-'
+        WALL = 'w'
+
+    list_ = []
+
+    _tile_list = {}
+
+    TILE_SIZE     = 32
+    total_tiles   = 1
+    H, V          = 1, 22
+
+    def __init__(self, x, y, tileType):
+
+        # AStar Variables
+        self.parent = None
+        self.H, self.G, self.F = 0, 0, 0
+
+        # Tile object specific variables
+        self.type = tileType
+        self.number = Tile.total_tiles
+
+        # Determine if this tile will be walkable or not
+        if tileType == Tile.Type.FLOOR:
+            self.walkable = True
+        elif tileType == Tile.Type.WALL:
+            self.walkable = False
+
+        pygame.Rect.__init__(self, (x, y), (Tile.TILE_SIZE, Tile.TILE_SIZE))
+        Tile.list_.append(self)
+
+        Tile._tile_list.update( { Tile.total_tiles : self } )
+        Tile.total_tiles += 1
+
+    @staticmethod
+    def get_tile(number):
+        # return Tile._tile_list[number]
+        for tile in Tile.list_:
+            if tile.number == number:
+                return tile
+
+    @staticmethod
+    def update(screen):
+
+        for tile in Tile.list_:
+            if tile.type == Tile.Type.FLOOR:
+                screen.blit(Tile.floor_tile_img, (tile.x, tile.y))
+
+            elif tile.type == Tile.Type.WALL:
+                screen.blit(Tile.wall_tile_img, (tile.x, tile.y))
